61b716a1419f2698b4f723dd04674429c12df9b0
신동화 <don92567@naver.com>
2025-08-24 15:12:54 +0900

    [chore] 누락 import추가

--- src/main/java/com/_1000meal/global/config/SecurityConfig.java
+++ src/main/java/com/_1000meal/global/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.HttpSecurityBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.ExceptionHandlingConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
