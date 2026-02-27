b1f8581aac0f6b82b12de95a1e140c6fc58c20e6
Sim-JuHeun <juheun9912@gmail.com>
2026-02-27 12:59:15 +0900

    [fix]마이그레이션 문 수정

--- src/main/resources/db/migration/V16__add_unique_notification_history_daily.sql
+++ src/main/resources/db/migration/V16__add_unique_notification_history_daily.sql
@@ -1,10 +1,10 @@
-ALTER TABLE notification_history
-    ADD COLUMN menu_group_id_norm BIGINT
-        GENERATED ALWAYS AS (IFNULL(menu_group_id, 0)) STORED;
-
-ALTER TABLE notification_history
-    ADD COLUMN week_key_norm VARCHAR(20)
-        GENERATED ALWAYS AS (IFNULL(week_key, '')) STORED;
-
-CREATE UNIQUE INDEX uq_notification_history_daily_norm
-    ON notification_history (sent_date, account_id, type, store_id, menu_group_id_norm, week_key_norm);
+-- ALTER TABLE notification_history
+--     ADD COLUMN menu_group_id_norm BIGINT
+--         GENERATED ALWAYS AS (IFNULL(menu_group_id, 0)) STORED;
+--
+-- ALTER TABLE notification_history
+--     ADD COLUMN week_key_norm VARCHAR(20)
+--         GENERATED ALWAYS AS (IFNULL(week_key, '')) STORED;
+--
+-- CREATE UNIQUE INDEX uq_notification_history_daily_norm
+--     ON notification_history (sent_date, account_id, type, store_id, menu_group_id_norm, week_key_norm);
